70f25d365df3e95701b6baaa64c7a524cea3b21b
micmor-m <micmor.m@gmail.com>
2020-08-03 18:41:03 +0000

    add ejs

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+//tells the Express app to use EJS as its templating engine
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
