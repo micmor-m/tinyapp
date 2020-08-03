3d9590f0f0b23a54a42191042dc5b174d7569ddd
micmor-m <micmor.m@gmail.com>
2020-08-03 19:10:42 +0000

    add urls_index.ejs and link

--- express_server.js
+++ express_server.js
@@ -18,6 +18,12 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls", (req, res) => {
+  //ejs template have to be always object
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
